1c9ac9cdec6a2d42edccde2d8b46ed1221493cab
Rana Mudasir <ranamudasir1000@gmail.com>
2023-10-25 01:26:18 +0500

    moved to controller

--- routes/api.php
+++ routes/api.php
@@ -23,46 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::match(['get','post'],'/signup',[userController::class,'signup'])->name('signup');
-Route::match(['get','post'],'/signup',function (Request $request){
-    if($request->isMethod('POST')){
-//        $request->validate([
-//            'fname' => 'max:255',
-//            'lname' => 'max:255',
-//            'bio' => 'max:500',
-//            'email' => 'unique:users,email|required|email|max:255',
-//            'password' => 'max:255',
-//            'imageSecurity' => 'max:255',
-//            'dname' => 'max:255',
-//            'facebook' => 'max:255',
-//            'instagram' => 'max:255',
-//            'linkedin' => 'max:255',
-//            'role' => '',
-//            'status' => ''
-//        ]);
-        $token = Str::random(30);
-        User::updateOrCreate([
-            "id" => $request->userId
-        ],[
-                'email' => $request->email,
-                'password' => $request->password,
-                'email_verified_at' => '0',
-                'rememberToken' => $token
-            ]);
-
-        $content = [
-            'subject' => 'Email Verification',
-            'body' => "$token"
-        ];
-
-        Mail::to("$request->email")->send(new SampleMail($content));
-        if(!empty($request->storyId)){
-            return response()->json(['type' => 'success','msg'=>'User Successfully Updated']);
-        }else{
-            return response()->json(['type' => 'success','msg'=>'User Successfully Added']);
-        }
-    }
-    else{
-        return response()->json(['msg'=>'Not Authorized']);
-    }
-})->name('signup');
+Route::match(['get','post'],'/signup',[userController::class,'signup'])->name('signup');
